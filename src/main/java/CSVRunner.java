bbc74e1bea7c89fe2d10b63ca3cd11dcd6a15ba3
mherbaghinyan <mbaghinyan@gmail.com>
2015-12-08 00:35:13 +0400

    added java files

--- src/main/java/CSVRunner.java
+++ src/main/java/CSVRunner.java
@@ -8,36 +8,11 @@ public class CSVRunner {
     private static final String userHome = System.getProperty("user.home");
     private static final String userDir = System.getProperty("user.dir");
 
-    private static final String CSV_SEMICOLON_SEPARATOR = ";";
 
     public CSVRunner() {
     }
 
-    public enum CsvFileDataType {
-        TOURNAMENTS("tournaments"),
-        PLAYERS("players");
-
-        private String value;
-
-        CsvFileDataType(String val) {
-            this.value = val;
-        }
-        public String getValue() {
-            return value;
-        }
-
-        @Override
-        public String toString() {
-            return "CsvFileDataType{" +
-                    "value='" + value + '\'' +
-                    '}';
-        }
-    }
-
     public static void main(String[] args) {
-
-        CSVRunner runner = new CSVRunner();
-
         ExcelReader.readOddTypesFromExcel(selectDirectory("email_list/"));
     }
 
