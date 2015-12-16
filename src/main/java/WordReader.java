8c7deede6e3465ad66fb17881cf4eb01914780d0
Mher Baghinyan <mbaghinyan@gmail.com>
2015-12-16 17:45:53 +0400

    Create WordReader.java

---
+++ src/main/java/WordReader.java
@@ -0,0 +1,43 @@
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.usermodel.IBodyElement;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * http://www.concretepage.com/apache-api/apache-poi-xwpf-read-ms-word-docx-header-footer-paragraph-table-example
+ * Created by Mher on 12/16/2015.
+ */
+public class WordReader {
+
+    public static void readWordDocument(String path) {
+        try {
+            FileInputStream fis = new FileInputStream(path);
+
+            XWPFDocument xdoc = new XWPFDocument(OPCPackage.open(fis));
+
+            Iterator<IBodyElement> bodyElementIterator = xdoc.getBodyElementsIterator();
+            while(bodyElementIterator.hasNext()) {
+                IBodyElement element = bodyElementIterator.next();
+                if("TABLE".equalsIgnoreCase(element.getElementType().name())) {
+                    List<XWPFTable> tableList =  element.getBody().getTables();
+                    for (XWPFTable table: tableList){
+                        System.out.println("Total Number of Rows of Table:"+table.getNumberOfRows());
+                        System.out.println(table.getText());
+                    }
+                }
+            }
+
+            fis.close();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
